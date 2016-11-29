7c51c2ef2cf6d19b5eb3da6d6beb09f6f81313eb
Chris Emerson <chris.emerson@sagical.com>
2016-11-29 14:44:12 -0700

    Added express Commented out most of main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
-import DataHelper = require('./Backend/Data/DataHelper');
-import Ingredient = require('./Backend/Components/Ingredient');
-import ConsoleInterface = require('./ConsoleInterface');
+import express = require('express');
+// import DataHelper = require('./Backend/Data/DataHelper');
+// import ConsoleInterface = require('./ConsoleInterface');
 
-var dataHelper = new DataHelper();
-var consoleInterface = new ConsoleInterface();
+// var dataHelper = new DataHelper();
+// var consoleInterface = new ConsoleInterface();
 var filePath = './Data/';
 var fileName = 'ingredient info.csv';
 
-//dataHelper.GetMatches(filePath, fileName, 'Taproot', () => console.log('Done'));
-//dataHelper.GetDiscoveries(filePath, fileName, 'Taproot');
-
-consoleInterface.UserQuestion();
+// consoleInterface.UserQuestion();
