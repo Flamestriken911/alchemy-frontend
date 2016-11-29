deeb1896a41e71af6794874961fa4d739246de1b
Chris Emerson <chris.emerson@sagical.com>
2016-11-29 15:46:27 -0700

    added initial routing to main.js

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 "use strict";
-const DataHelper = require('./Backend/Data/DataHelper');
-const ConsoleInterface = require('./ConsoleInterface');
-var dataHelper = new DataHelper();
-var consoleInterface = new ConsoleInterface();
-var filePath = './Data/';
-var fileName = 'ingredient info.csv';
-//dataHelper.GetMatches(filePath, fileName, 'Taproot', () => console.log('Done'));
-//dataHelper.GetDiscoveries(filePath, fileName, 'Taproot');
-consoleInterface.UserQuestion();
+const express = require('express');
+const DataHandler = require('./Backend/Data/DataHandler');
+// import ConsoleInterface = require('./ConsoleInterface');
+// var consoleInterface = new ConsoleInterface();
+var dataHandler = new DataHandler();
+var app = express();
+// consoleInterface.UserQuestion();
+app.get('/ingredients', function (req, res) {
+    dataHandler.GetUserListOrDefault(null, (err, list) => {
+        if (err) {
+            //TODO: real error handling
+            console.log('There was an error retrieving data');
+            res.send(null);
+        }
+        else {
+            res.send(JSON.stringify(list));
+        }
+    });
+});
+app.get('/', function (req, res) {
+    res.send('hello');
+});
+app.listen(3000, function () {
+    console.log('Listening on port 3000...');
+});
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
 import express = require('express');
-// import DataHelper = require('./Backend/Data/DataHelper');
+import DataHandler = require('./Backend/Data/DataHandler');
 // import ConsoleInterface = require('./ConsoleInterface');
 
-// var dataHelper = new DataHelper();
 // var consoleInterface = new ConsoleInterface();
-var filePath = './Data/';
-var fileName = 'ingredient info.csv';
+var dataHandler = new DataHandler();
+var app = express();
 
 // consoleInterface.UserQuestion();
+app.get('/', function(req, res) {
+    res.send('hello');
+}).get('/ingredients', function(req, res) {
+    dataHandler.GetUserListOrDefault(null, (err, list) => {
+        if(err){
+            //TODO: real error handling
+            console.log('There was an error retrieving data');
+            res.sendStatus(500);
+        } else {
+            res.json(list);
+        }
+    })
+})
+
+app.listen(3001, function() {
+    console.log('Listening on port 3001...');
+})
